f6a8a6856dcaac3a8f64e90030028a033f5ff9b4
Aditya Pai <aditya.a.pai.tech@gmail.com>
2026-07-16 14:24:49 +0530

    building a simple script to clean raw csv data

---
+++ portfolio_analyzer/src/data_management/data_ingestion.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Actual data to work with, loaded from external csv file.
+root_df = pd.read_csv("data_dir/raw_transactions.csv")
+
+"""
+    Following functions to perform:
+    1. Raw strings of dates converted to datetime objects
+    2. Handling Nulls: NaN values replaced with '0.0'
+    3. String Parsing with Regex
+"""
+
+def datetime_conversion():
+    global root_df
+    root_df["Timestamp"] = pd.to_datetime(root_df["Timestamp"])
+
+def nan_manage():
+    global root_df
+    root_df["Tx_Fee"] = root_df["Tx_Fee"].fillna(0.0)
+
+def parsing_regex():
+    global root_df
