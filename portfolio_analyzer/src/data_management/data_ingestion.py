fc06eb5184e8581a3bbf08692615c77813a362fa
Aditya Pai <aditya.a.pai.tech@gmail.com>
2026-07-19 21:48:10 +0530

    made modifications in data reading related functions

--- portfolio_analyzer/src/data_management/data_ingestion.py
+++ portfolio_analyzer/src/data_management/data_ingestion.py
@@ -13,11 +13,18 @@ root_df = pd.read_csv("data_dir/raw_transactions.csv")
 
 def datetime_conversion():
     global root_df
-    root_df["Timestamp"] = pd.to_datetime(root_df["Timestamp"])
+    try:
+        root_df["Timestamp"] = pd.to_datetime(root_df["Timestamp"])
+    except Exception as e:
+        return f"Error: {e}"
+
 
 def nan_manage():
     global root_df
-    root_df["Tx_Fee"] = root_df["Tx_Fee"].fillna(0.0)
+    try:
+        root_df["Tx_Fee"] = root_df["Tx_Fee"].fillna(0.0)
+    except Exception as e:
+        return f"Error: {e}"
 
 def regex_parsing():
     global root_df
