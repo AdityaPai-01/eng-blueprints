84cbf2aaee1beff59cdcc0f99be2467ecc4a1355
Aditya Pai <aditya.a.pai.tech@gmail.com>
2026-08-03 20:38:49 +0530

    created the task object blueprint

--- todolist_app/src/model/task.py
+++ todolist_app/src/model/task.py
@@ -1,3 +1,12 @@
 class Task:
-    def __init__(self):
-        pass
+    def __init__(self, taskname):
+        self.taskname: str = taskname
+        self.is_completed: bool = False
+
+    def __str__(self):
+        if self.is_completed:
+            return f'✅: {self.taskname}'
+        return f'⭕: {self.taskname}'
+
+    def mark_complete(self):
+        self.is_completed = True
